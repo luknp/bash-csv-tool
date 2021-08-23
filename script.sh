eb5f06f86f225140e4e03d1d5c93bea3f237e2fe
luknp <luk.npl@gmail.com>
2021-08-23 23:00:56 +0200

    feat: add functions

--- script.sh
+++ script.sh
@@ -3,6 +3,30 @@
 export SOURCE=""
 export REGEX_EXP=""
 
+function create_dir_if_doesnt_exist(){
+    if [[ ! -d $1 ]]
+    then
+        mkdir $1
+    fi
+}
+
+function check_if_dir_empty(){
+    if [ "$(ls -A $1)" ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function check_if_file_exist(){
+    if [ ! -f $1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 while test $# -gt 0; do
     case "$1" in
         -h|--help)
@@ -42,5 +66,12 @@ while test $# -gt 0; do
     esac
 done
 
-echo $SOURCE
-echo $REGEX_EXP
+SOURCE_DIR_PATH="source/"
+DEST_DIR_PATH="destiny/"
+
+declare -a DIR_ARRAY=($SOURCE_DIR_PATH $DEST_DIR_PATH)
+for DIR in "${DIR_ARRAY[@]}"
+do
+    create_dir_if_doesnt_exist ${DIR}
+    rm -rf ${DIR}/*
+done
